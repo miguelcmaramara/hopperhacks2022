0f296eab2935139a488b0fa81a44d1eb480bf3d2
miguelcmaramara <miguelcmaramara@gmail.com>
2022-02-12 14:17:04 -0500

    added styling to quiz content

--- src/components/InTrees.js
+++ src/components/InTrees.js
@@ -1,9 +1,12 @@
 import "./css/InTrees.css"
+import TreeSec from "./TreeSec";
+
 
 function InTrees(props){
     return(
         <div className="InTrees">
-            <h1>hello World</h1>
+            <TreeSec color="red" size="10%"/>
+            <TreeSec color="orange" size="30%"/>
 
         </div>
     )
