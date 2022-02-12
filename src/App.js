8134cf88248d2f02097044430955555ad66c2843
miguelcmaramara <miguelcmaramara@gmail.com>
2022-02-12 12:29:15 -0500

    more boilerplate with template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactComponent from './components/TEMPLATE'
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and don't save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <ReactComponent></ReactComponent>
     </div>
   );
 }
---
+++ src/components/TEMPLATE.js
@@ -0,0 +1,12 @@
+import './css/TEMPLATE.css';
+
+function ReactComponent(props){
+    return(
+        <div className="ReactComponent">
+            <h1>in fun React component</h1>
+        </div>
+    );
+}
+
+
+export default ReactComponent;
