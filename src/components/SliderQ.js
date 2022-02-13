123a050f01cae742537c5945e26c6a40d5bcd0c0
adam nebylitsa <adamnebylitsa@gmail.com>
2022-02-12 21:51:37 -0500

    Fixed Slider label error

--- src/components/SliderQ.js
+++ src/components/SliderQ.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 export default function SliderQ(props){
     useEffect(() => {
-    document.getElementById('textInput').value=document.getElementById("slide").value; 
+    document.getElementById('textInput'+props.num).value=document.getElementById("slide").value; 
     });
     const [val, setVal] = useState(0);
     return(
         <div>
             <h4>{props.question}</h4><br></br>
-            <input id="slide" type="range" min={props.min} max={props.max} onChange={handleChange}/>
-            <input type="text" id="textInput" value= {val}/>
+            <input num = {props.num} id="slide" type="range" min={props.min} max={props.max} onChange={handleChange}/>
+            <input type="text" id={"textInput"+props.num} value= {val}/>
         </div>
     )
     
 }
 function handleChange(event) {
-    document.getElementById('textInput').value=event.target.value;
+    document.getElementById('textInput'+event.target.getAttribute("num")).value=event.target.value;
   }
